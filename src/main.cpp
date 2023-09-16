ba48b7b77e5d2da4251b31e8eca60d76181e0d20
Andrea Ghidini <andrea.ghidini@looptribe.com>
2023-09-16 16:50:11 +0200

    License preamble

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-/* This file is part of Lobster Moving Head, Copyright (C) 2023 Andrea Ghidini.
+/* This file is part of Lobster Mini Moving Head, Copyright (C) 2023 Andrea Ghidini.
  *
  * Lobster Mini Moving Head is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
