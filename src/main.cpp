126f5f141e476d208c39766a087552fddbbc6818
Andrea Ghidini <andrea.ghidini@looptribe.com>
2023-11-01 15:57:18 +0100

    RGBW Dimmer

--- src/main.cpp
+++ src/main.cpp
@@ -16,15 +16,13 @@
 
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <Servo.h>
 
 #include <ArtnetWiFi.h>
 
-#define PAN_PIN D1
-#define TILT_PIN D2
-#define R_PIN D5
-#define G_PIN D6
-#define B_PIN D7
+#define R_PIN D1
+#define G_PIN D2
+#define B_PIN D5
+#define W_PIN D6
 
 #define STATUS_PIN D4
 
@@ -35,34 +33,25 @@
 #define ARTNET_SUBNET 0
 
 #define DMX_CHANNEL_BASE 0
+
 #define DMX_CHANNEL_R 0
 #define DMX_CHANNEL_G 1
 #define DMX_CHANNEL_B 2
-#define DMX_CHANNEL_BRIGHTNESS 3
-#define DMX_CHANNEL_STROBE 4
-#define DMX_CHANNEL_PAN 5
-#define DMX_CHANNEL_TILT 6
-#define DMX_CHANNEL_SPEED 7
+#define DMX_CHANNEL_W 3
+#define DMX_CHANNEL_BRIGHTNESS 4
+#define DMX_CHANNEL_STROBE 5
 
 #define STROBE_MIN 0.2f
 #define STROBE_MAX 50.f
-
-#define MAX_SPEED 180.f
 
 #define statusOn() digitalWrite(STATUS_PIN, LOW)
 #define statusOff() digitalWrite(STATUS_PIN, HIGH)
 #define statusBlink(ms) do { statusOn(); delay(ms); statusOff(); delay(ms); } while (0)
 
-Servo panServo;
-Servo tiltServo;
-
-float r = 0.f, g = 0.f, b = 0.f;
+float r = 0.f, g = 0.f, b = 0.f, w = 0.f;
 float brightness = 0.f;
 uint8_t strobe = 0;
-float targetPan = 0.f, targetTilt = 0.f;
-float panSpeed = MAX_SPEED, tiltSpeed = MAX_SPEED; // per sec
 
-float currentPan = targetPan, currentTilt = targetTilt;
 int strobeOn = 0;
 
 unsigned long lastMicros;
@@ -79,6 +68,8 @@ void artnetCallback(const uint8_t* data, const uint16_t size)
         g = static_cast<float>(data[DMX_CHANNEL_BASE + DMX_CHANNEL_G]);
     if (size > DMX_CHANNEL_BASE + DMX_CHANNEL_B)
         b = static_cast<float>(data[DMX_CHANNEL_BASE + DMX_CHANNEL_B]);
+    if (size > DMX_CHANNEL_BASE + DMX_CHANNEL_W)
+        w = static_cast<float>(data[DMX_CHANNEL_BASE + DMX_CHANNEL_W]);
     if (size > DMX_CHANNEL_BASE + DMX_CHANNEL_BRIGHTNESS)
         brightness = static_cast<float>(data[DMX_CHANNEL_BASE + DMX_CHANNEL_BRIGHTNESS]) / 255.f;
     if (size > DMX_CHANNEL_BASE + DMX_CHANNEL_STROBE)
@@ -87,12 +78,6 @@ void artnetCallback(const uint8_t* data, const uint16_t size)
         const auto x = pow(2.f, (strobe - 10.f) / 235.f) * .5f - .5f;
         currentStrobePeriod = static_cast<unsigned long>(1000.f / (x * (STROBE_MAX - STROBE_MIN) + STROBE_MIN));
     }
-    if (size > DMX_CHANNEL_BASE + DMX_CHANNEL_PAN)
-        targetPan = static_cast<float>(data[DMX_CHANNEL_BASE + DMX_CHANNEL_PAN]) * 180.f / 255.f;
-    if (size > DMX_CHANNEL_BASE + DMX_CHANNEL_TILT)
-        targetTilt = static_cast<float>(data[DMX_CHANNEL_BASE + DMX_CHANNEL_TILT]) * 180.f / 255.f;
-    if (size > DMX_CHANNEL_BASE + DMX_CHANNEL_SPEED)
-        panSpeed = tiltSpeed = static_cast<float>(255 - data[DMX_CHANNEL_BASE + DMX_CHANNEL_SPEED]) * MAX_SPEED / 255.f;
 }
 
 void waitWiFi()
@@ -122,11 +107,8 @@ void setup()
     pinMode(R_PIN, OUTPUT);
     pinMode(G_PIN, OUTPUT);
     pinMode(B_PIN, OUTPUT);
+    pinMode(W_PIN, OUTPUT);
     pinMode(STATUS_PIN, OUTPUT);
-    panServo.attach(PAN_PIN);
-    tiltServo.attach(TILT_PIN);
-    panServo.write(static_cast<int>(currentPan));
-    tiltServo.write(static_cast<int>(currentTilt));
     WiFi.begin();
     WiFi.mode(WIFI_STA);
 
@@ -134,8 +116,8 @@ void setup()
 
     artnet.begin(ARTNET_NET, ARTNET_SUBNET);
     artnet.subscribe(ARTNET_UNIVERSE, artnetCallback);
-    artnet.shortname("LS Moving Head");
-    artnet.longname("Lobster Mini Moving Head");
+    artnet.shortname("LS Dimmer");
+    artnet.longname("Lobster Mini RGBW Dimmer");
     artnet.nodereport("");
 
     Serial.println("Setup completed.");
@@ -145,18 +127,6 @@ void setup()
 
 void update(unsigned long deltaUs, float deltaTime)
 {
-    // Serial.printf("%.1f\t%.1f\t%.1f\t%.3f\n", targetPan - currentPan, targetPan, currentPan, panSpeed * deltaTime);
-
-    if (std::abs(targetPan - currentPan) >= panSpeed * deltaTime)
-        currentPan += std::copysign(panSpeed * deltaTime, targetPan - currentPan);
-    else
-        currentPan = targetPan;
-
-    if (std::abs(targetTilt - currentTilt) >= tiltSpeed * deltaTime)
-        currentTilt += std::copysign(tiltSpeed * deltaTime, targetTilt - currentTilt);
-    else
-        currentTilt = targetTilt;
-
     if (strobe < 10)
         strobeOn = 0;
     else if (strobe > 245)
@@ -179,9 +149,8 @@ void loop()
 
     update(elapsed, deltaTime);
 
-    panServo.write(static_cast<int>(currentPan));
-    tiltServo.write(static_cast<int>(currentTilt));
     analogWrite(R_PIN, static_cast<int>(r * brightness) * strobeOn);
     analogWrite(G_PIN, static_cast<int>(g * brightness) * strobeOn);
     analogWrite(B_PIN, static_cast<int>(b * brightness) * strobeOn);
+    analogWrite(W_PIN, static_cast<int>(w * brightness) * strobeOn);
 }
